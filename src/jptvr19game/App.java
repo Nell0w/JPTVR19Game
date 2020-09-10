2eb11b9a4115a69a594b3c4d68c37cfd66b157a5
pupil <pupil@ivkhk.ee>
2020-09-10 11:15:55 +0300

    Работающая программа с диапозоном и attempt

--- src/jptvr19game/App.java
+++ src/jptvr19game/App.java
@@ -15,17 +15,21 @@ import java.util.Random;
 public class App {
     public void run (){
         System.out.println("Привет.");
-        System.out.println("Программа задумала число от 0 до 5, угадай:");
+        System.out.println("Загадай максимальный и минимальный диапозон: ");
         Random random = new Random();
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Минимальный: ");
+        int userNumber1 = scanner.nextInt();
+        System.out.println("Максимальный: ");
+        int userNumber2 = scanner.nextInt();
+        System.out.println("Программа загадала число от " + userNumber1 + " до " + userNumber2 + " ,угадай: ");
         int attempt = 0;
         do {
-            
-        
-            int myNumber = random.nextInt(5 - 0 + 1)+ 0;
-            Scanner scanner = new Scanner(System.in);
+            int myNumber = random.nextInt((userNumber2 - userNumber1) + 1)+ userNumber1;
+            System.out.println(myNumber);
             int gamerNumber = scanner.nextInt();
             if(myNumber == gamerNumber){
-                System.out.println("Ты выиграл!");
+                System.out.println("Ты выиграл! " + myNumber);
                 break;
             }else{
                 if (attempt > 1){
